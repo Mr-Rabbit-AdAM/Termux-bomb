29ce283e223fbddfa25b4882f53c58adf56e5071
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-10-13 23:29:54 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ pkg install termux-api -y > /dev/null 2>1
 echo - e"\e[1;32m  Installed Termux API \e[0m"
 echo ""
 
-cp termux-api.apk cd /sdcard/ 
+cp Termux-api.apk cd /sdcard/ 
 echo -e "\e[1;32m You have installed the Termux api apk on your storage and installed it\e[0m"
 echo ""
 echo -e "\e[1;32m The installation process is complete \e[0m"
